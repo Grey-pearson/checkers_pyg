576dd14b3f535708aa2e9466aacd2da6f21ca1a5
grey <greytpearson@gmail.com>
2024-06-25 15:16:28 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,21 @@
-import pygame
+import pygame, sys, time
 from sprite_sheet import Spritesheet
-
-pygame.init()
-DISPLAY_W, DISPLAY_H = 320, 340
-screen = pygame.display.set_mode((DISPLAY_W,DISPLAY_H))
-pygame.display.set_caption("checkers")
-clock = pygame.time.Clock()
-running = True
+from settings import *
 
 my_sprite_sheet = Spritesheet('CheckersSpriteSheet.png')
 
-# you can string frames in this list format to get animnations stored in one variable
-# white_puck = [my_sprite_sheet.parse_sprite('white_puck.png')]  
-# 'pucks'
-white_puck = my_sprite_sheet.parse_sprite('white_puck.png') 
-black_puck = my_sprite_sheet.parse_sprite('black_puck.png')
-# squares
-white_square = my_sprite_sheet.parse_sprite('white_square.png')
-black_square = my_sprite_sheet.parse_sprite('black_square.png')
-red_square = my_sprite_sheet.parse_sprite('red_square.png')
-# selector
-selector_TL = my_sprite_sheet.parse_sprite('selector_TL.png')
-selector_TM = my_sprite_sheet.parse_sprite('selector_TM.png')
-selector_TR = my_sprite_sheet.parse_sprite('selector_TR.png')
-selector_LM = my_sprite_sheet.parse_sprite('selector_LM.png')
-selector_RM = my_sprite_sheet.parse_sprite('selector_RM.png')
-selector_BL = my_sprite_sheet.parse_sprite('selector_BL.png')
-selector_BM = my_sprite_sheet.parse_sprite('selector_BM.png')
-selector_BR = my_sprite_sheet.parse_sprite('selector_BR.png')
-# text
-white_to_play = my_sprite_sheet.parse_sprite('white_to_play.png')
-black_to_play = my_sprite_sheet.parse_sprite('black_to_play.png')
+
 
 class Game():
     def __init__(self):
         # to make assigning pieces to specific coordinates 
 
-    
+        pygame.init()
+        
+        self.screen = pygame.display.set_mode((DISPLAY_W,DISPLAY_H))
+        pygame.display.set_caption("checkers")
+        self.clock = pygame.time.Clock()
+        running = True
 
 
 
@@ -50,46 +29,31 @@ class Game():
             pass
 
 
-while running:
+    def run(self):
+        last_time = time.time()
+        while True:
 
-    screen.fill((75,150,100))
+            # delta time
+            dt = time.time() - last_time
+            last_time = time.time()
 
-    # game code;
-    screen.blit(white_square, (32, 0))
-    screen.blit(black_square, (0, 0))
-    screen.blit(white_puck, (0, 0))
-    screen.blit(black_puck, (32, 0))
-    screen.blit(red_square, (32*2, 0))
+            # event loop
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
 
-    screen.blit(white_square, (32*3, 0))
-    screen.blit(black_square, (32*4, 0))
-    screen.blit(white_square, (32*5, 0))
-    screen.blit(black_square, (32*3, 32))
-    screen.blit(white_square, (32*4, 32))
-    screen.blit(black_square, (32*5, 32))
-    screen.blit(white_square, (32*3, 32*2))
-    screen.blit(black_square, (32*4, 32*2))
-    screen.blit(white_square, (32*5, 32*2))
+            # updating game
+            self.all_sprites.update(dt)
+            self.player.player_constraint()
 
-    screen.blit(black_puck, (32*4, 32))
-
-    screen.blit(selector_TL, (32*3, 0))
-    screen.blit(selector_TM, (32*4, 0))
-    screen.blit(selector_TR, (32*5, 0))
-    screen.blit(selector_LM, (32*3, 32))
-    screen.blit(selector_RM, (32*5, 32))
-    screen.blit(selector_BL, (32*3, 32*2))
-    screen.blit(selector_BM, (32*4, 32*2))
-    screen.blit(selector_BR, (32*5, 32*2))
+            self.display_surface.blit(self.background, (0, 0))
+            # this is needed to draw sprite to screen frfr
+            self.all_sprites.draw(self.display_surface)
+            # update window
+            pygame.display.update()
 
 
-    screen.blit(white_to_play, (32*1, 32*4))
-    screen.blit(black_to_play, (32*1, 32*6))
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    pygame.display.update()
-    pygame.display.flip()
-    clock.tick()
-pygame.quit()
+if __name__ == "__main__":
+    game = Game()
+    game.run()
