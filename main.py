29a441b1e44b31ee6ffa79bd619a7367b4f2271a
grey <greytpearson@gmail.com>
2024-06-25 10:57:47 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ import pygame
 import sprite_sheet as Spritesheet
 
 pygame.init()
-
-screen = pygame.display.set_mode((480,400))
+DISPLAY_W, DISPLAY_H = 480, 400
+screen = pygame.display.set_mode((DISPLAY_W,DISPLAY_H))
 pygame.display.set_caption("checkers")
 clock = pygame.time.Clock()
 running = True
 
 my_sprite_sheet = Spritesheet('CheckersSpriteSheet.png')
-white_puck = [my_sprite_sheet.parse]
+white_puck = [my_sprite_sheet.parse_sprite('white_puck.png')]
+
+
 
 # sprite_sheet_image = pygame.image.load('CheckersSpriteSheet.png').convert_alpha()
 # 22 blocks long
@@ -21,6 +23,7 @@ while running:
     screen.fill((75,150,100))
 
     # game code;
+    screen.blit(white_puck[0], (0, DISPLAY_H - 128))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
