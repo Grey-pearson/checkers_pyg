e92896fc42b95c6e336c8cfbda9da3c21366d8d6
grey <greytpearson@gmail.com>
2024-06-25 11:13:07 -0600

    Update sprite_sheet.py

--- sprite_sheet.py
+++ sprite_sheet.py
@@ -20,4 +20,6 @@ class Spritesheet:
         sprite = self.data['frames'][name]['frame']
         x, y, w, h = sprite['x'], sprite['y'], sprite['w'], sprite ['h']
         image = self.get_sprite(x, y, w, h)
+        # scale it with meta data but idk how to do that yet
+        image = pygame.transform.scale(image, ( sprite[w] * self.meta_data.scale, sprite[h]*self.meta_data.scale))
         return image
