a24c4ae22f62641c4d4dcf59bd6ddb1385217b74
grey <greytpearson@gmail.com>
2024-06-25 17:34:25 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pygame, sys, time
 from sprite_sheet import Spritesheet
-from settings import *
+# from settings import *
 
-my_sprite_sheet = Spritesheet('CheckersSpriteSheet.png')
+DISPLAY_W, DISPLAY_H = 320, 384
+RED = (78,40,46)
+
 
 class Game():
     def __init__(self):
@@ -12,18 +14,52 @@ class Game():
         
         self.screen = pygame.display.set_mode((DISPLAY_W,DISPLAY_H))
         pygame.display.set_caption("checkers")
+        self.screen.fill(RED)
         self.clock = pygame.time.Clock()
+        # self.my_sprite_sheet = Spritesheet('CheckersSpriteSheet.png')
+        my_sprite_sheet = Spritesheet('CheckersSpriteSheet.png')
 
+        # you can string frames in this list format to get animnations stored in one variable
+        # white_puck = [my_sprite_sheet.parse_sprite('white_puck.png')]  
+        # 'pucks'
+        self.white_puck = my_sprite_sheet.parse_sprite('white_puck.png') 
+        self.black_puck = my_sprite_sheet.parse_sprite('black_puck.png')
+        # squares
+        self.white_square = my_sprite_sheet.parse_sprite('white_square.png')
+        self.black_square = my_sprite_sheet.parse_sprite('black_square.png')
+        self.red_square = my_sprite_sheet.parse_sprite('red_square.png')
+        # selector
+        self.selector_TL = my_sprite_sheet.parse_sprite('selector_TL.png')
+        self.selector_TM = my_sprite_sheet.parse_sprite('selector_TM.png')
+        self.selector_TR = my_sprite_sheet.parse_sprite('selector_TR.png')
+        self.selector_LM = my_sprite_sheet.parse_sprite('selector_LM.png')
+        self.selector_RM = my_sprite_sheet.parse_sprite('selector_RM.png')
+        self.selector_BL = my_sprite_sheet.parse_sprite('selector_BL.png')
+        self.selector_BM = my_sprite_sheet.parse_sprite('selector_BM.png')
+        self.selector_BR = my_sprite_sheet.parse_sprite('selector_BR.png')
+        # text
+        self.white_to_play = my_sprite_sheet.parse_sprite('white_to_play.png')
+        self.black_to_play = my_sprite_sheet.parse_sprite('black_to_play.png')
 
+        self.create_board()
 
-        def coordinates(self, x, y):
-            x *= 32
-            y *= 32
-            calculated_coordinates = (x,y)
-            return(calculated_coordinates)
+    def coordinates(self, x, y):
+        x *= 32
+        y *= 32
+        calculated_coordinates = (x,y)
+        return(calculated_coordinates)
 
-        def create_board(self):
-            pass
+    def create_board(self):
+        # this works
+        # self.screen.blit(self.white_puck, self.coordinates(1,1))
+        for col in range(8):
+            for row in range(8):
+                if (row+1) % 2 == 1:
+                    # print((col+1) % 2, '   ', col)
+                    self.screen.blit(self.black_square, self.coordinates(row+1,col+2))
+                else:
+                    # print((col+1) % 2, "  else  ", col)
+                    self.screen.blit(self.white_square, self.coordinates(row+1,col+2))
 
 
     def run(self):
@@ -31,7 +67,7 @@ class Game():
         while True:
 
             # delta time
-            dt = time.time() - last_time
+            # dt = time.time() - last_time
             last_time = time.time()
 
             # event loop
@@ -41,12 +77,12 @@ class Game():
                     sys.exit()
 
             # updating game
-            self.all_sprites.update(dt)
-            self.player.player_constraint()
+            # self.all_sprites.update(dt)
+            # self.player.player_constraint()
 
-            self.display_surface.blit(self.background, (0, 0))
+            # self.display_surface.blit(self.background, (0, 0))
             # this is needed to draw sprite to screen frfr
-            self.all_sprites.draw(self.display_surface)
+            # self.all_sprites.draw(self.display_surface)
             # update window
             pygame.display.update()
 
